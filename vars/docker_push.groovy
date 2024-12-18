a390c07a3df2eec1d4e9feee5113a55b57563420
kumawat2222 <129721544+kumawat2222@users.noreply.github.com>
2024-12-18 20:11:52 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,5 +2,5 @@ def call(String Project, String ImageTag, String dockerhubuser){
  withCredentials([usernamePassword(credentialsId: 'dockerhubCred', passwordVariable: 'dockerHubPass', usernameVariable: 'dockerHubUser')]) {
       sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
   }
-  sh "docker push ${dockerHubUser}/${Project}:${ImageTag}"
+  sh "docker push docker.io/${dockerHubUser}/${Project}:${ImageTag}"
 }
